12875ad3edf2b7f3f568458c1f419c08d51cd03b
Akhmetov Adil <qweasdzxc0091@mail.ru>
2024-02-19 18:44:22 +0300

    commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Миссия Колонизация Марса"
+
+
+@app.route("/index")
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route("/promotion")
+def promotion():
+    return """Человечество вырастает из детства.</br></br>Человечеству мала одна планета.</br></br>\
+        Мы сделаем обитаемыми безжизненные пока планеты.</br></br>\
+            И начнем с Марса!</br></br>Присоединяйся!"""
+
+
+@app.route("/image_mars")
+def image_mars():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/planet.webp')}" alt="здесь должна была быть картинка, но не нашлась">'
+                    <div class="alert alert-dark" role="alert">
+                      Человечество вырастет из детсва.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
+
+
+if __name__ == "__main__":
+     app.run(port=8080, host='127.0.0.1')
